4edb86bd4ebc76d9c99af5fad4bd43b01e2f8f3f
Pieter Beulque <pieterbeulque@gmail.com>
2018-06-27 13:58:32 +0200

    Fix whitespace in gulp/config.js

--- templates/gulp/config.js
+++ templates/gulp/config.js
@@ -2,36 +2,36 @@ const buildPath = './www/assets';
 const sourcePath = './gulp';
 
 module.exports = {
-  css: {
-    src: `${sourcePath}/css/*.css`,
-    dest: `${buildPath}/css`,
-    watch: [
-      `${sourcePath}/css/**/*.css`,
-      `${sourcePath}/css/*.css`,
-    ],
-  },
-  favicons: {
-    src: {
-      png: `${sourcePath}/favicons/favicon.png`,
-      svg: `${sourcePath}/favicons/favicon.svg`,
-    },
-    dest: `${buildPath}/favicons`,
-  },
-  icons: {
-    src: `${sourcePath}/icons/*.svg`,
-    dest: `${buildPath}/icons`,
-    filename: 'icons.svg',
-  },
-  images: {
-    src: `${sourcePath}/images/**`,
-    dest: `${buildPath}/images`,
-  },
-  js: {
-    src: `${sourcePath}/js/*.js`,
-    dest: `${buildPath}/js`,
-    watch: [
-      `${sourcePath}/js/**/*.js`,
-      `${sourcePath}/js/*.js`,
-    ],
-  },
+	css: {
+		src: `${sourcePath}/css/*.css`,
+		dest: `${buildPath}/css`,
+		watch: [
+			`${sourcePath}/css/**/*.css`,
+			`${sourcePath}/css/*.css`,
+		],
+	},
+	favicons: {
+		src: {
+			png: `${sourcePath}/favicons/favicon.png`,
+			svg: `${sourcePath}/favicons/favicon.svg`,
+		},
+		dest: `${buildPath}/favicons`,
+	},
+	icons: {
+		src: `${sourcePath}/icons/*.svg`,
+		dest: `${buildPath}/icons`,
+		filename: 'icons.svg',
+	},
+	images: {
+		src: `${sourcePath}/images/**`,
+		dest: `${buildPath}/images`,
+	},
+	js: {
+		src: `${sourcePath}/js/*.js`,
+		dest: `${buildPath}/js`,
+		watch: [
+			`${sourcePath}/js/**/*.js`,
+			`${sourcePath}/js/*.js`,
+		],
+	},
 };
